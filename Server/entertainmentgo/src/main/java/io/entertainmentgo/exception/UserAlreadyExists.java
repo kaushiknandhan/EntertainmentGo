dd8700a08711816082739b9ef60a460f29d85950
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-04-17 22:45:25 -0700

    feat(EntertainmentGo): modify user entity exceptions

--- Server/entertainmentgo/src/main/java/io/entertainmentgo/exception/UserAlreadyExists.java
+++ Server/entertainmentgo/src/main/java/io/entertainmentgo/exception/UserAlreadyExists.java
@@ -3,7 +3,7 @@ package io.entertainmentgo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.FOUND,reason="User already exists")
+@ResponseStatus(code=HttpStatus.FOUND)
 public class UserAlreadyExists extends Exception {
 
 	private static final long serialVersionUID = 1L;
--- Server/entertainmentgo/src/main/java/io/entertainmentgo/exception/UserNotFound.java
+++ Server/entertainmentgo/src/main/java/io/entertainmentgo/exception/UserNotFound.java
@@ -3,7 +3,7 @@ package io.entertainmentgo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.NOT_FOUND,reason="No user present in DataBase")
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
 public class UserNotFound extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
